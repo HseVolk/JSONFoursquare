452c27ae27846e672c58f4a1f71f29f54e770c49
HseVolk <yuravolk2006@mail.ru>
2012-11-06 23:44:04 +0400

    Initial Commit

---
+++ JSONFoursquare/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  JSONFoursquare
+//
+//  Created by Волк on 06.11.12.
+//  Copyright (c) 2012 BI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ JSONFoursquare/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  JSONFoursquare
+//
+//  Created by Волк on 06.11.12.
+//  Copyright (c) 2012 BI. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
